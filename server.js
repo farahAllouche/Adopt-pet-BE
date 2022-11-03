2f6d7120d2cc69ee1d07221b381c8c5b98906a99
farah <farahallouche327@gmail.com>
2022-11-03 16:13:32 +0100

    update to deploy

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT =  process.env.PORT || 4000
 app.use(
     cors({
       credentials: true,
-      origin: "http://localhost:3000",
+      origin: "https://reliable-kitten-e91b98.netlify.app/",
     })
   );
 
