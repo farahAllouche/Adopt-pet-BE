dc2951d0f1e82dbd26894e3449215b312bf1a2d6
farahAllouche <farahallouche327@gmail.com>
2024-10-02 09:36:29 +0100

    console cookies

--- Middleware/deserializeUser.js
+++ Middleware/deserializeUser.js
@@ -1,48 +1,42 @@
-const jwt = require('jsonwebtoken')
-const {checkRefreshToken} = require('../Session/session.controller')
-const { verifyJWT, signJWT} = require('../Session/jwt.utils')
+const jwt = require("jsonwebtoken");
+const { checkRefreshToken } = require("../Session/session.controller");
+const { verifyJWT, signJWT } = require("../Session/jwt.utils");
 
+function deserializeUser(req, res, next) {
+  const { accessToken, refreshToken } = req.cookies;
+  console.log("access", accessToken);
+  console.log(refreshToken);
 
-
-function deserializeUser (req, res, next) {
-  
-    const { accessToken, refreshToken } = req.cookies;
-    
-    
-    if (!accessToken) {
+  if (!accessToken) {
+    return next();
+  }
+  const { payload: user, expired } = verifyJWT(accessToken);
+  console.log("user", user, expired);
+  if (user) {
+    req.user = { name: user.name, userId: user.userId };
+    return next();
+  }
+  let newUser;
+  expired &&
+    checkRefreshToken(refreshToken).then((token) => {
+      newUser = token ? verifyJWT(token).payload : null;
+      if (!newUser) {
         return next();
       }
-      const { payload : user, expired } = verifyJWT(accessToken);
-      //console.log('user', expired);
-      if (user) {
-          req.user = {name : user.name, userId : user.userId};
-          return next();
-        }
-        let newUser;
-        expired && checkRefreshToken(refreshToken).then((token)=> {
-          newUser=  token? verifyJWT(token).payload : null;
-          if (!newUser) {
-            
-            return next();
-          }
-    
-        const newAccessToken = signJWT({name : newUser.name, userId : newUser.userId}, "15m")
-    
-      
-        res.cookie("accessToken", newAccessToken, {
-          maxAge: 3.154e10, 
-          httpOnly: true,
-        });
-    
-        req.user = {name : newUser.name, userId : newUser.userId};
-        return next()
-          
-        })
 
-    
+      const newAccessToken = signJWT(
+        { name: newUser.name, userId: newUser.userId },
+        "15m"
+      );
 
+      res.cookie("accessToken", newAccessToken, {
+        maxAge: 3.154e10,
+        httpOnly: true,
+      });
 
-
+      req.user = { name: newUser.name, userId: newUser.userId };
+      return next();
+    });
 }
 
-module.exports = {deserializeUser }
+module.exports = { deserializeUser };
