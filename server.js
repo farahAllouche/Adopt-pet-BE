179ed66041204c373e989ab9f034efd5d56c8eb1
farahAllouche <farahallouche327@gmail.com>
2024-10-02 08:40:27 +0100

    disable cors

--- server.js
+++ server.js
@@ -1,34 +1,31 @@
-const express = require("express")
-const app = express()
-require('dotenv').config()
-require('./database')
-const { deserializeUser } = require('./Middleware/deserializeUser')
-const SessionRouter =require('./Session/session.router')
-const PetRouter =require('./Pet/pet.router')
-const LostPetRouter =require('./LostPet/lost.router')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+require("./database");
+const { deserializeUser } = require("./Middleware/deserializeUser");
+const SessionRouter = require("./Session/session.router");
+const PetRouter = require("./Pet/pet.router");
+const LostPetRouter = require("./LostPet/lost.router");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-const PORT =  process.env.PORT || 4000
-
+const PORT = process.env.PORT || 4000;
 
 app.use(
-    cors({
-      credentials: true,
-      origin: "http://localhost:3000",
-    })
-  );
+  cors({
+    /*credentials: true,
+      origin: "http://localhost:3000",*/
+  })
+);
 
-  app.use(cookieParser());
-  
-  app.use(express.json());
-  
-app.use('/', SessionRouter)
-app.use('/pets', PetRouter)
-app.use('/lostPet', LostPetRouter)
+app.use(cookieParser());
 
+app.use(express.json());
 
-app.listen(PORT, ()=>{
+app.use("/", SessionRouter);
+app.use("/pets", PetRouter);
+app.use("/lostPet", LostPetRouter);
 
-    console.log("App running on port ", PORT)
-})
+app.listen(PORT, () => {
+  console.log("App running on port ", PORT);
+});
